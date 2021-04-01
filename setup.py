6102ce46dc06f8e822ac5c0c6cdb502608031ab0
Aritra Roy Gosthipaty <aritra.born2fly@gmail.com>
2021-04-01 13:21:05 +0530

    "" -> ''
    Changing the type of quotes to standardize all.

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ REQUIRED_PKGS = [
 setup(
     name=project_name,
     version=version,
-    description="A documentation generator.",
-    author="Artira Roy Gosthipaty",
-    author_email="aritra.born2fly@gmail.com",
+    description='A documentation generator.',
+    author='Artira Roy Gosthipaty',
+    author_email='aritra.born2fly@gmail.com',
     url='http://github.com/ariG23498/docugen',
     download_url='https://github.com/ariG23498/docugen/tags',
     install_requires=REQUIRED_PKGS,
